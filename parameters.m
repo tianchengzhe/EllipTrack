6524c6a8fc20326933d382c332946917a6745c90
tianchengzhe <c.z.tian@gmail.com>
2019-05-24 16:22:44 -0600

    Change Comments

--- parameters.m
+++ parameters.m
@@ -9,11 +9,9 @@ function [ all_parameters ] = parameters()
 %% GLOBAL_SETTING
 % Parameters used by all tracker modules.
 
-% [Essential] nuc_raw_image_path: Path to the folder with all the captured
-% images of the nuclear channel (TIFF or ND2 formats).
-% [Essential] nd2_frame_range: Range of frames each ND2 file stores. If the
-% captured images are stored in the TIFF format, this variable should be
-% empty.
+% [Essential] nuc_raw_image_path: Path to the folder containing images of
+% the nuclear channel.
+% [Essential] nd2_frame_range: Range of frames each ND2 file stores.
 nuc_raw_image_path = {'Z:/projects/tracking_code/20181115/Raw/'};
 nd2_frame_range = [1, 288];
 % DO NOT CHANGE THE SCRIPTS BELOW
@@ -29,11 +27,11 @@ end
 % [Essential] valid_wells: Movies being tracked.
 valid_wells = allcomb(2:2, 3:3, 1:1);
 
-% [Essential] cmosoffset_path: Path to the .mat file storing the camera
-% dark noises (CMOS Offset).
+% [Essential] cmosoffset_path: Path to the MAT file storing the camera dark
+% noises (CMOS Offset).
 cmosoffset_path = 'Z:/microscope_mat_files/nikon1_matfiles/cmosoffset.mat';
 
-% [Essential] nuc_bias_path: Path to the .mat file storing the illumination
+% [Essential] nuc_bias_path: Path to the MAT file storing the illumination
 % bias (Bias) of the nuclear channel.
 nuc_bias_path = 'Z:/microscope_mat_files/nikon1_matfiles/CFP.mat';
 
@@ -46,12 +44,11 @@ nuc_signal_name = 'CFP';
 % [Essential] nuc_biomarker_name: Name of the measured nuclear marker.
 nuc_biomarker_name = 'H2B';
 
-% [Optional] if_global_correction: Whether to perform global jitter
-% correction.
+% [Optional] if_global_correction: Indicator variable of whether to perform
+% global jitter correction.
 if_global_correction = 0;
 
-% [Essential] output_path: Path to the folder storing the output of the
-% tracker.
+% [Essential] output_path: Path to the folder storing the outputs.
 output_path = 'Z:/projects/tracking_code/20181115/MCF10A/myversion/results/2_3_1/';
 
 % Assemble into a struct variable
@@ -64,64 +61,62 @@ global_setting = struct('nuc_raw_image_path', {nuc_raw_image_path}, 'nd2_frame_r
 % Parameters used by Segmentation
 
 % Part 1. Non-Specific Parameters
-% [Optional] if_active_contour: Whether to perform the Active Contour step.
+% [Optional] if_active_contour: Indicator variable of whether to run Active
+% Contour.
 if_active_contour = 1;
 
-% [Optional] if_watershed: Whether to perform the Watershed step.
+% [Optional] if_watershed: Indicator variable of whether to run Watershed.
 if_watershed = 1;
 
-% [Optional] if_seg_correction: Whether to use training data to correct
-% mistakes of segmentation.
+% [Optional] if_seg_correction: Indicator variable of whether to run
+% Correction with Training Datasets.
 if_seg_correction = 0;
 
-% [Essential]: if_print_mask: Whether to output the mask before ellipse
-% fitting. 1 indicates outputting and 0 indicates not.
-% [Essential]: mask_path: Path to the folder storing the mask.
+% [Essential]: if_print_mask: Indicator variable of whether to generate the
+% mask before Ellipse Fitting.
+% [Essential]: mask_path: Path to the folder storing the masks.
 if_print_mask = 1;
 mask_path = 'Z:/projects/tracking_code/20181115/MCF10A/myversion/mask/';
 
-% [Essential] if_print_ellipse_movie: Whether to visualize the ellipse
-% fitting results by 'ellipse movie' where the fitted ellipses are overlaid
-% on the nuclear images. 1 indicates visualizing and 0 indicates not.
-% [Essential] ellipse_movie_path: Path to the folder storing ?ellipse
-% movie?.
+% [Essential] if_print_ellipse_movie: Indicator variable of whether to
+% generate 'Ellipse Movie' where fitted ellipses are overlaid on the
+% nuclear images.
+% [Essential] ellipse_movie_path: Path to the folder storing the 'Ellipse
+% Movies'.
 if_print_ellipse_movie = 1;
 ellipse_movie_path = 'Z:/projects/tracking_code/20181115/MCF10A/myversion/ellipse_movie/';
 
-% [Essential] if_save_seg_info: Whether to save the ellipse fitting results
-% of every frame ('seg info').
-% [Essential] seg_info_path: Path to the folder storing ?seg info?.
+% [Essential] if_save_seg_info: Indicator variable of whether to save the
+% features of ellipses in each frame ('seg info').
+% [Essential] seg_info_path: Path to the folder storing the 'seg info'.
 if_save_seg_info = 1;
 seg_info_path = 'Z:/projects/tracking_code/20181115/MCF10A/myversion/seg_info/';
 
-% [Optional] nuc_radius: Blob detection only. The average radius (in
-% pixels) of a nucleus.
+% [Optional] nuc_radius: Average radius (in pixels) of a nucleus.
 nuc_radius = 12;
 
-% [Optional] max_hole_size_for_fill: The maximal area (in pixels) of a hole
-% within a component to fill.
+% [Optional] max_hole_size_for_fill: Maximal area (in pixels) of a hole to
+% fill in.
 max_hole_size_for_fill = 200;
 
-% [Optional] min_component_size_for_nucleus: Used in both methods. The
-% minimal area (in pixels) of a component to be considered as a nucleus.
-% Any component with a smaller area will be removed.
+% [Optional] min_component_size_for_nucleus: Minimal area (in pixels) of a
+% component to contain a nucleus.
 min_component_size_for_nucleus = 25;
 
 % Part 2. Image Binarization
-% [Optional] blurradius: Used in both methods. Radius (in pixels) of disk
-% for image smoothing.
+% [Optional] blurradius: Both methods. Radius (in pixels) of disk for
+% smoothing.
 blurradius = 3; 
 
-% [Essential] if_log: Used in both methods. Whether to perform
-% log-transform to the image.
+% [Essential] if_log: Both methods. Indicator variable of whether to
+% log-transform the images.
 if_log = 1;
 
-% [Essential] if_blob_detection: Used in both methods. Whether to perform
-% blob detection or not.
+% [Essential] if_blob_detection: Both methods. Indicator variable of
+% whether to run Blob Detection.
 if_blob_detection = 0;
 
-% [Important] blob_threshold: Blob detection only. Threshold of hessian.
-% Should be a negative number.
+% [Important] blob_threshold: Blob Detection only. Threshold of Hessian.
 blob_threshold = -0.1;
 
 % Assemble into a struct variable 
@@ -129,24 +124,23 @@ image_binarization_para = struct('blurradius', blurradius, 'if_log', if_log, ...
     'if_blob_detection', if_blob_detection, 'blob_threshold', blob_threshold);
 
 % Part 3. Active Contour
-% [Optional] blurradius: Radius (in pixels) of disk for mask smoothing.
+% [Optional] blurradius: Radius (in pixels) of disk for smoothing.
 blurradius = 2;
 
-% [Essential] if_log: Whether to perform log-transform to the image.
+% [Essential] if_log: Indicator variable of whether to log-transform the
+% images.
 if_log = 1;
 
-% [Essential] if_global: Whether to apply the global option of active
-% contour algorithm or not.
+% [Essential] if_global: Indicator variable of whether to run the Global
+% method.
 if_global = 0;
 
 % Assemble into a struct variable
 active_contour_para = struct('blurradius', blurradius, 'if_log', if_log, 'if_global', if_global);
 
 % Part 4. Watershed
-% [Optional] max_thresh_component_size: The maximal area (in pixels) of an
-% eroded component to be added to the refined mask.
-% [Optional] min_thresh_component_size: The minimal area (in pixels) of an
-% eroded component to be added to the refined mask.
+% [Optional] max_thresh_component_size: Maximal area (in pixels) of a peak.
+% [Optional] min_thresh_component_size: Minimal area (in pixels) of a peak.
 max_thresh_component_size = 25;
 min_thresh_component_size = 0;
 
@@ -156,7 +150,8 @@ watershed_para = struct('max_thresh_component_size', max_thresh_component_size, 
 
 % Part 5. Ellipse Fitting
 % [Optional] k, thd1, thd2, thdn, C, T_angle, sig, Endpoint, Gap_size:
-% Parameters in Zafari et al 2015.
+% Parameters in Zafari et al 2015. Refer to the original publication for
+% more information.
 k = 5; 
 thd1 = 10; 
 thd2 = 25;
@@ -168,7 +163,7 @@ Endpoint = 1;
 Gap_size = 1; 
 
 % [Optional] min_ellipse_perimeter: Minimal perimeter (in pixels) of an
-% ellipse
+% ellipse.
 min_ellipse_perimeter = 5; 
 
 % [Optional] min_ellipse_area: Minimal area (in pixels) of an ellipse.
@@ -183,7 +178,7 @@ ellipse_para = struct('k', k, 'thd1', thd1, 'thd2', thd2, 'thdn', thdn, 'C', C, 
     'min_ellipse_area', min_ellipse_area, 'max_major_axis', max_major_axis);
 
 % Part 6. Correction of Segmentation Mistakes
-% [Essential] training_data_path: Paths to the training data.
+% [Essential] training_data_path: Paths to the training datasets.
 training_data_path = {'Z:/projects/tracking_code/20181115/MCF10A/myversion/mat_files/2_1_1_CFP_training_data_21_100.mat';
     'Z:/projects/tracking_code/20181115/MCF10A/myversion/mat_files/2_1_1_CFP_training_data_126_175.mat';
     'Z:/projects/tracking_code/20181115/MCF10A/myversion/mat_files/2_1_1_CFP_training_data_201_250.mat'};
@@ -194,19 +189,19 @@ end
 % DO NOT CHANGE THE SCRIPTS ABOVE
 
 % [Optional] min_ellipse_area_twocells: Minimal area (in pixels) of an
-% ellipse to perform the k-means algorithm.
+% ellipse for splitting.
 min_ellipse_area_twocells = 20;
 
 % [Optional] max_fraction_mismatch: Maximal fraction (between 0 and 1) of
 % mismatch between two k-means runs.
 max_fraction_mismatch = 0.1;
 
-% [Optional] min_no_cell_prob: Threshold probability (between 0 and 1) of
-% containing no nucleus.
+% [Optional] min_no_cell_prob: Minimal probability (between 0 and 1) of
+% containing no nucleus for removal.
 min_no_cell_prob = 0.9;
 
-% [Optional] min_two_cells_prob: Threshold probability (between 0 and 1) of
-% containing two nuclei.
+% [Optional] min_two_cells_prob: Minimal probability (between 0 and 1) of
+% containing two nuclei for splitting.
 min_two_cells_prob = 0.9;
 
 % Assemble into a struct variable
@@ -228,7 +223,8 @@ segmentation_para = struct('if_active_contour', if_active_contour, ...
 %% TRACK_PARA
 % Parameters used for track linking
 
-% [Essential] training_data_path: Paths to the training data.
+% Part 1. Predict Probabilities
+% [Essential] training_data_path: Paths to the training datasets.
 training_data_path = {'Z:/projects/tracking_code/20181115/MCF10A/myversion/mat_files/2_1_1_CFP_training_data_21_100.mat';
     'Z:/projects/tracking_code/20181115/MCF10A/myversion/mat_files/2_1_1_CFP_training_data_126_175.mat';
     'Z:/projects/tracking_code/20181115/MCF10A/myversion/mat_files/2_1_1_CFP_training_data_201_250.mat'};
@@ -238,30 +234,31 @@ for i=1:length(training_data_path)
 end
 % DO NOT CHANGE THE SCRIPTS ABOVE
 
-% [Optional] empty_prob: Probability of an event (between 0 and 1) if the
-% training data is absent.
+% [Optional] empty_prob: Probability of an event (between 0 and 1) if no
+% training samples are provided.
 empty_prob = 0.0001;
 
-% [Important] if_switch_off_before_mitosis: Whether to ignore the
-% probability of mitotic cell ('Before M') when evaluating the score for
-% mitosis
-% [Optional] if_switch_off_after_mitosis: Whether to ignore the
-% probability of newly born cells ('After M') when evaluating the score for
-% mitosis
+% [Important] if_switch_off_before_mitosis: Indicator variable of whether
+% to ignore the probability of being an mitotic cell (Before M) during
+% mitosis detection.
+% [Optional] if_switch_off_after_mitosis: Indicator variable of whether to
+% ignore the probability of being a newly born cell (After M) during
+% mitosis detection.
 if_switch_off_before_mitosis = 1;
 if_switch_off_after_mitosis = 0;
 
-% [Optional] if_similarity_for_migration: Whether to account for ellipse
-% similarities when calculating migration probabilities
+% [Optional] if_similarity_for_migration: Indicator variable of whether to
+% consider ellipse similarity when computing migration probabilities.
 if_similarity_for_migration = 1;
 
-% [Important] migration_sigma: The standard deviation (in pixels) of
-% migration in one frame and one direction. If NaN is chosen, the value
-% will be inferred from the training data.
+% [Important] migration_sigma: Standard deviation (in pixels) of random
+% walk in one frame and one direction. If NaN is chosen, the value will be
+% inferred from the training datasets.
 migration_sigma = 4;
 
-% [Optional] max_migration_distance_fold: Maximal distances an ellipse can
-% travel in each direction and each frame.
+% [Optional] max_migration_distance_fold: Maximal distances (expressed as
+% folds of migration_sigma) an ellipse can travel in one frame and one
+% direction.
 max_migration_distance_fold = 20;
 
 % [Optional] likelihood_nonmigration: Null probability (between 0 and 1)
@@ -272,58 +269,60 @@ likelihood_nonmigration = 0.001;
 % migrating in/out of the field of view.
 min_inout_prob = 0.0001;
 
-% [Optional] max_gap: Maximal value of 'gap' (in the number fo frames) for
-% migration. Gap = number of frames to skip + 1
+% [Optional] max_gap: Maximal gap (in frames) of migration. Gap = number of
+% frames to skip + 1.
 max_gap = 1;
 
-% [Optional] skip_penalty: Penalty score for a track to skip one frame.
+% Part 2. Construct Cell Tracks
+% [Optional] skip_penalty: Penalty score for skipping one frame.
 skip_penalty = 10;
 
-% [Optional] multiple_cells_penalty: Penalty score for an ellipse to
-% contain two nuclei.
+% [Optional] multiple_cells_penalty: Penalty score for two cells
+% co-existing in one ellipse.
 multiple_cells_penalty = 5;
 
 % [Optional] min_mitosis_prob: Minimal probability (between 0 and 1) of
-% mitotic and newly born cells.
+% mitotic and newly born cells for mitosis detection.
 min_mitosis_prob = 0;
 
-% [Optional] max_num_tracks: Maximal number of tracks to search.
+% [Optional] max_num_tracks: Maximal number of tracks to construct.
 max_num_tracks = Inf;
 
-% [Optional] min_track_score: Minimal score of a track to accept
+% [Optional] min_track_score: Minimal score of a track.
 min_track_score = 2;
 
 % [Optional] min_track_score_per_step: Minimal score of a track between two
 % neighboring frames.
 min_track_score_per_step = -15;
 
-% [Optional] max_recorded_link: Number of migration events to keep for each
-% ellipse. 
+% [Optional] max_recorded_link: For each ellipse, maximal number of
+% migration events to keep.
 max_recorded_link = 5;
 
-% [Optional] min_swap_score: Minimal score for swaping tracks in
-% post-processing
+% Part 3. Post-Processing
+% [Optional] min_swap_score: Minimal score for swapping cell tracks.
 min_swap_score = 2;
 
-% [Optional] fixation_min_prob_before_mitosis: Minimal probability of the
-% mitotic cell (Before M) to define a missing mitosis event.
-% [Optional] fixation_min_prob_after_mitosis: Minimal probability of the
-% newly born cells (After M) to define a missing mitosis event.
+% [Optional] fixation_min_prob_before_mitosis: Minimal probability of
+% mitotic cells (Before M) for mitosis detection.
+% [Optional] fixation_min_prob_after_mitosis: Minimal probability of newly
+% born cells (After M) for mitosis detection.
 fixation_min_prob_before_mitosis = 0.5;
 fixation_min_prob_after_mitosis = 0.5;
 
-% [Optional] min_track_length: Minimal length (in the number of frames) of
-% a track.
+% [Optional] min_track_length: Minimal length (in frames) of a valid track.
 min_track_length = 10;
 
-% [Optional] max_num_frames_to_skip: Maximal number of frames a track can
-% skip.
+% [Optional] max_num_frames_to_skip: Maximal number of frames a valid track
+% can skip.
 max_num_frames_to_skip = 2;
 
-% [Essential] if_print_vistrack: Whether to visualize the tracks by
-% plotting the 'vistrack' movie.
-% [Essential] vistrack_path: Path to the folder storing the 'vistrack'
-% movie.
+% Part 4. Visualization
+% [Essential] if_print_vistrack: Indicator variable of whether to generate
+% 'Vistrack Movies' where fitted ellipses are overlaid on the nuclear
+% images and Cell Track IDs are displayed next to the ellipses they mapped
+% to.
+% [Essential] vistrack_path: Path to the folder storing 'Vistrack Movies'.
 if_print_vistrack = 1;
 vistrack_path = 'Z:/projects/tracking_code/20181115/MCF10A/myversion/vistrack/';
 
@@ -347,6 +346,7 @@ track_para = struct('training_data_path', {training_data_path}, 'empty_prob', em
 % other words, this will NOT include the nuclear marker (H2B) which is used
 % to perform tracking
 
+% Part 1. Signal Channels
 % [Essential] additional_signal_names: Names of the signal channels.
 additional_signal_names = {'mCherry'};
 
@@ -354,7 +354,7 @@ additional_signal_names = {'mCherry'};
 % the signal channels.
 additional_biomarker_names = {'CDK2'};
 
-% [Essential] additional_raw_image_paths: Paths to the folder storing the captured
+% [Essential] additional_raw_image_paths: Paths to the folder containing
 % images of the signal channels.
 additional_raw_image_paths = {{'Z:/projects/tracking_code/20181115/Raw/'}};
 % USE THE FOLLOWING SCRIPTS IF USING ND2 FILES OR ALL TIFF FILES ARE STORED IN THE SAME FOLDER
@@ -373,18 +373,19 @@ for i=1:length(additional_raw_image_paths)
 end
 % DO NOT CHANGE THE SCRIPTS ABOVE
 
-% [Essential] additional_bias_paths: Paths to the Matlab data file (.mat)
-% storing the bias information for each signal channel.
+% [Essential] additional_bias_paths: Paths to the MAT files storing the
+% illumination bias of the signal channels.
 additional_bias_paths = {'Z:/microscope_mat_files/nikon1_matfiles/mCherry.mat'};
 % DO NOT CHANGE THE SCRIPTS BELOW
 for i=1:length(additional_bias_paths)
     additional_bias_paths{i} = adjust_path(additional_bias_paths{i});
 end
 % DO NOT CHANGE THE SCRIPTS ABOVE
-% [Essential] if_compute_cyto_ring: Indicator variables to determine
-% whether to extract signals in the cytoplasmic ring.
+% [Essential] if_compute_cyto_ring: Indicator variables of whether to
+% extract the signals in the cytoplasmic ring.
 if_compute_cyto_ring = [1];
 
+% Part 2. ROI
 % [Optional] cyto_ring_inner_size: Minimal distance (in pixels) between the
 % region of cytoplasmic ring and the ellipse contour.
 % [Optional] cyto_ring_outer_size: Maximal distance (in pixels) between the
@@ -395,8 +396,8 @@ cyto_ring_inner_size = 1;
 cyto_ring_outer_size = 4;
 nuc_outer_size = 1;
 
-% [Optional] foreground_dilation_size: Maximal distance (in pixels) between
-% background and a nucleus.
+% [Optional] foreground_dilation_size: Minimal distance (in pixels) between
+% the image background and a nucleus.
 foreground_dilation_size = 20;
 
 % [Optional] intensity_percentile: Measured percentile (between 0 and 100)
